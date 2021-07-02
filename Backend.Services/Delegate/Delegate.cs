24790dcf055d189f64a4343ef40ead0b2fea5572
thetechrebel <stevedumbura@gmail.com>
2021-07-02 10:43:27 +0200

    Added an example of a delegate

---
+++ Backend.Services/Delegate/Delegate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Backend.Services.Delegate
+{
+    public class Delegate
+    {
+        string input;
+        public Delegate()
+        {
+            input = "input";
+            this.DoSomething(ToJson);
+            this.DoSomething(ToXML);
+            this.DoSomething(ToFormData);
+        }
+
+
+        public delegate string ReturnData();
+
+        public string ToJson()
+        {
+            return input + "/";
+        }
+        public string ToXML()
+        {
+            return input + "<>";
+        }
+        public string ToFormData()
+        {
+            return input + ":" + input;
+        }
+
+        public void DoSomething(ReturnData data)
+        {
+            Console.WriteLine(data);
+        }
+    }
+}
